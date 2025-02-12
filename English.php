85e55f95972f769dc2fe74c2e0caf784c17a00d9
Kitten Films <kittenapps@icloud.com>
2025-02-12 18:39:50 +0500

    Add files via upload

---
+++ English.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ENG</title>
+	<?php include_once '../head.php'; ?>
+</head>
+
+<body><center>
+	<?php include_once 'headerE.php'; ?>
+<div class="wrapper">
+	<header>
+    <img src="../images/background.png" class="background"/>
+	
+	<h1 class="title">Healthy life</h1><br/><h1 class="title2">You were made to live a healthy life</h1>
+	</header>
+	<section>
+		<table width="80%">
+		<tbody class="body">
+    <div>
+	<tr class="wide" id="water">
+      <td width="50%"><img src="../images/water2.png" width="50%" alt="" class="left"/></td>
+      <td width="50%"><p><span class="T">Water</span><br/>My education and training completes my coaching arsenal – allowing me to provide you, the client, with the tools and methods you need to achieve your goals.</p><br/><a href="water.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="wide" id="activity">
+      <td><p><span class="T">Activity</span><br/>Extensive experience allows me to guide you. My client success stories reflect my ability to, time after time, help clients improve  their health and achieve a higher level of life satisfaction.</p><br/><a href="activity.php"><input type="button" value="Learn more" class="button"></a></td>
+      <td><img src="../images/activity2.png" width="50%" alt="" class="right"/></td>
+    </tr>
+    <tr class="wide" id="victory">
+      <td><img src="../images/victory2.png" width="50%" alt="" class="center"/></td>
+      <td><p><span class="T">Victory</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="victory.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="wide" id="worth">
+      <td><p><span class="T">Worth</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="worth.php"><input type="button" value="Learn more" class="button"></a></td>
+      <td><img src="../images/worth2.png" width="50%" alt="" class="center"/></td>
+    </tr>
+    <tr class="wide" id="rest">
+      <td><img src="../images/rest2.png" width="50%" alt="" class="center"/></td>
+      <td><p><span class="T">Rest</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="rest.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="wide" id="environment">
+      <td><p><span class="T">Environment</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="environment.php"><input type="button" value="Learn more" class="button"></a></td>
+      <td><img src="../images/environment2.png" width="50%" alt="" class="center"/></td>
+    </tr>
+    <tr class="wide" id="air">
+      <td><img src="../images/air2.png" width="50%" alt="" class="center"/></td>
+      <td><p><span class="T">Air</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="air.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="wide" id="sunshine">
+      <td><p><span class="T">Sunshine</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="sunshine.php"><input type="button" value="Learn more" class="button"></a></td>
+      <td><img src="../images/sunshine2.png" width="50%" alt="" class="center"/></td>
+    </tr>
+    <tr class="wide" id="nutrition">
+      <td><img src="../images/nutrition2.png" width="50%" alt="" class="center"/></td>
+      <td><p><span class="T">Nutrition</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="nutrition.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="wide" id="dream">
+      <td><p><span class="T">Dream</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="dream.php"><input type="button" value="Learn more" class="button"></a></td>
+      <td><img src="../images/dream2.png" width="50%" alt="" class="right"/></td>
+    </tr>
+    <tr class="wide" id="trust">
+      <td><img src="../images/trust2.png" width="50%" alt="" class="center"/></td>
+      <td><p><span class="T">Trust</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="trust.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr></div>
+	<!--size change-->
+	<div>
+	<tr class="narrow" id="water">
+      <td width="50%"><img src="../images/water2.png" width="50%" alt="" class="left"/></td></tr><tr class="narrow">
+      <td width="50%"><p><span class="T">Water</span><br/>My education and training completes my coaching arsenal – allowing me to provide you, the client, with the tools and methods you need to achieve your goals.</p><br/><a href="water.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="narrow" id="activity">
+	  <td><img src="../images/img.png" width="50%" alt="" class="right"/></td></tr><tr class="narrow">
+      <td><p><span class="T">Activity</span><br/>Extensive experience allows me to guide you. My client success stories reflect my ability to, time after time, help clients improve  their health and achieve a higher level of life satisfaction.</p><br/><a href="activity.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="narrow" id="victory">
+      <td><img src="../images/victory2.png" width="50%" alt="" class="center"/></td></tr><tr class="narrow">
+      <td><p><span class="T">Victory</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="victory.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="narrow" id="worth">
+	  <td><img src="../images/img.png" width="50%" alt="" class="center"/></td></tr><tr class="narrow">
+      <td><p><span class="T">Worth</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href=".worth.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="narrow" id="rest">
+      <td><img src="../images/rest2.png" width="50%" alt="" class="center"/></td></tr><tr class="narrow">
+      <td><p><span class="T">Rest</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="rest.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="narrow" id="environment">
+	  <td><img src="../images/img.png" width="50%" alt="" class="center"/></td></tr><tr class="narrow">
+      <td><p><span class="T">Environment</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="environment.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="narrow" id="air">
+      <td><img src="../images/air2.png" width="50%" alt="" class="center"/></td></tr><tr class="narrow">
+      <td><p><span class="T">Air</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="air.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="narrow" id="sunshine">
+	  <td><img src="../images/img.png" width="50%" alt="" class="center"/></td></tr><tr class="narrow">
+      <td><p><span class="T">Sunshine</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="sunshine.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="narrow" id="nutrition">
+      <td><img src="../images/nutrition2.png" width="50%" alt="" class="center"/></td></tr><tr class="narrow">
+      <td><p><span class="T">Nutrition</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="nutrition.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="narrow" id="dream">
+	  <td><img src="../images/img.png" width="50%" alt="" class="right"/></td></tr><tr class="narrow">
+      <td><p><span class="T">Dream</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="dream.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr>
+    <tr class="narrow" id="trust">
+      <td><img src="../images/trust2.png" width="50%" alt="" class="center"/></td></tr><tr class="narrow">
+      <td><p><span class="T">Trust</span><br/>Learn how to have victory over all that is harmful and practice moderation in all that is good.</p><br/><a href="trust.php"><input type="button" value="Learn more" class="button"></a></td>
+    </tr></div>
+    <tr>
+      <th colspan="2" style="width: 100%; "><a href="https://www.youtube.com/@saghlamhayat" target="_blank" id="youtube"><img src="../images/youtube2.png" alt="" class="you"/></a></th>
+    </tr>
+  </tbody>
+</table>
+	</section>
+	<script src="../index.js"></script>
+</div></center></body>
+</html>
