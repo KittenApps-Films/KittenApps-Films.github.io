da452d36c480b67f0c89134fc586203d97c3a3e3
Kitten Films <kittenapps@icloud.com>
2025-02-22 13:17:31 +0500

    Update up.js

--- up.js
+++ up.js
@@ -4,7 +4,7 @@ async function upload() {
   const owner = 'KittenApps-Films';
   const repo = 'KittenApps-Films.github.io';
   const path = 'amogus.html';
-  const auth = '{MY ACCESS TOKEN HERE}';
+  const auth = '{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}';
 
   const existingFile = await (await fetch(
     `https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
