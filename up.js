818096d1bba1557ed72fd0fb3c1734d04be2f914
Kitten Films <kittenapps@icloud.com>
2025-02-22 17:29:19 +0500

    Update up.js

--- up.js
+++ up.js
@@ -3,8 +3,8 @@ const octokit = new Octokit({
   auth: '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'
 })
 await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
-  owner: 'OWNER',
-  repo: 'REPO',
+  owner: 'KittenApps-Films',
+  repo: 'KittenApps-Films.github.io',
   event_type: 'on-demand-test',
   client_payload: {
     unit: false,
