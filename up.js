6a64da79d50939a2d974e66a668ea08ee65e559d
Kitten Films <kittenapps@icloud.com>
2025-02-22 17:19:33 +0500

    Create up.js

---
+++ up.js
@@ -0,0 +1,16 @@
+import { Octokit } from "https://cdn.skypack.dev/octokit";
+const octokit = new Octokit({
+  auth: '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'
+})
+await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
+  owner: 'OWNER',
+  repo: 'REPO',
+  event_type: 'on-demand-test',
+  client_payload: {
+    unit: false,
+    integration: true
+  },
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
