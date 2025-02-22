dade488e222d2c36b96dc42589d72bcacf5ba7a2
Kitten Films <kittenapps@icloud.com>
2025-02-22 17:59:09 +0500

    Update up.js

--- up.js
+++ up.js
@@ -1,13 +1,18 @@
 import { Octokit } from "https://esm.sh/@octokit/core";
 
-export const octokit = new Octokit({
+const octokit = new Octokit({
   auth: "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",
-});
+})
 
-const repo = await octokit.request(
-  "GET /users/{username}/repos{?type,sort,direction,per_page,page}",
-  {
-    username: "KittenApps-Films",
+await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
+  owner: 'OWNER',
+  repo: 'REPO',
+  event_type: 'on-demand-test',
+  client_payload: {
+    unit: false,
+    integration: true
+  },
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
   }
-);
-console.log(repo);
+})
