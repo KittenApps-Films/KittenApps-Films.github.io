70140e4b9494a7927dc625c41bf5b4c8125e8597
Kitten Films <kittenapps@icloud.com>
2025-02-22 17:32:11 +0500

    Update up.js

--- up.js
+++ up.js
@@ -1,16 +1,13 @@
 import { Octokit } from "https://cdn.skypack.dev/octokit";
-const octokit = new Octokit({
-  auth: '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'
-})
-await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
-  owner: 'KittenApps-Films',
-  repo: 'KittenApps-Films.github.io',
-  event_type: 'on-demand-test',
-  client_payload: {
-    unit: false,
-    integration: true
-  },
-  headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
+
+export const octokit = new Octokit({
+  auth: "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",
+});
+
+const repo = await octokit.request(
+  "GET /users/{username}/repos{?type,sort,direction,per_page,page}",
+  {
+    username: "your_username",
   }
-})
+);
+console.log(repo);
