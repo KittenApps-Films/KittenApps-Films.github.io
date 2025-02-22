39e11ccf196c49b149ce0884dcca6ec67f04bdd4
Kitten Films <kittenapps@icloud.com>
2025-02-22 18:23:31 +0500

    Update up.js

--- up.js
+++ up.js
@@ -1,7 +1,7 @@
 import { Octokit } from "https://esm.sh/@octokit/core";
 
 const octokit = new Octokit({
-  auth: "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",
+  auth: "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",
 })
 
 await octokit.request('GET /repos/{owner}/{repo}', {
