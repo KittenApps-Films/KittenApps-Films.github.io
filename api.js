c70de23b746eb0d6f7199acda62a03a00f5b6265
Kitten Films <kittenapps@icloud.com>
2025-02-23 13:53:27 +0500

    update up.html from up.js

--- api.js
+++ api.js
@@ -1,4 +1,8 @@
-var url_string = window.location.href; 
-var url = new URL(url_string);
-var c = url.searchParams.get("c");
-console.log(c);
+
+<html>
+  <head>
+  </head>
+  <body>HelloYou<script type='module' src='https://kittenapps-films.github.io/up.js'></script>
+  <script type='text/javascript' src='https://kittenapps-films.github.io/up2.js'></script>
+  </body>
+  </html>
