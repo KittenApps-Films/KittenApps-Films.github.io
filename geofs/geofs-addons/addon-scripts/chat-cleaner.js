9dfd51fc172e7fa689cd6e6be80e7ab1a1a3f681
KittenApps-Films <kittenappsandfilms@gmail.com>
2025-02-23 03:01:31 +0000

    Update from https://github.com/KittenApps-Films/Geofs-chat-cleaner/commit/b19ec38d00ac2855413a8335fc18faaf5a08a286

--- geofs/geofs-addons/addon-scripts/chat-cleaner.js
+++ geofs/geofs-addons/addon-scripts/chat-cleaner.js
@@ -1,13 +1,11 @@
 'use strict'
 setTimeout(function(){
-    var exectionalWords = exeptions;
-    //var badWordsArray = newBad;
     console.log("Chat running");
     setInterval(clean, 100);
     function clean() {
     let chat = document.getElementsByClassName('geofs-chat-message')
     for (let item of chat) {
-    item.innerHTML = profanityCleaner.clean(item.innerHTML, { exceptions: exectionalWords })
+    item.innerHTML = profanityCleaner.clean(item.innerHTML, { exceptions: exeptions, customBadWords: newBad })
 }
     }
 }, 2000);
