e5bc54baa3b012201e4a9ef298afa2f8b9346eb5
Kitten Films <kittenapps@icloud.com>
2025-02-23 13:57:53 +0500

    Update up.js

--- up.js
+++ up.js
@@ -14,6 +14,12 @@ var url_string = window.location.href;
 var url = new URL(url_string);
 var d = url.searchParams.get("d");
 
+var url_string = window.location.href; 
+var url = new URL(url_string);
+var r = url.searchParams.get("r");
+
+if (r == true) {
+
 import { Octokit } from "https://esm.sh/@octokit/core";
 
 var one = "github_pat_11BO3GZVQ0BEH77fHoiWF7_iuWSG0S"
@@ -22,7 +28,7 @@ var two = "0ubQtn31vuMDAyfXu1LtRM3EgHhVXqXTc2p5YYM7IJP6bBLd7eoR"
 const octokit = new Octokit({
   auth: one + two,
 })
-
+var s = true
 try { 
   var old = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
   owner: 'KittenApps-Films',
@@ -32,6 +38,10 @@ try {
     'X-GitHub-Api-Version': '2022-11-28'
   }
 })
+  } catch {
+  false
+}
+
 
 var newFile = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
   owner: 'KittenApps-Films',
@@ -39,10 +49,11 @@ var newFile = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}',
   path: d,
   message: 'update up.html from up.js',
   content: btoa(content),
+  if (s == true) {
   sha: old.data.sha,
+  }
   headers: {
     'X-GitHub-Api-Version': '2022-11-28'
   }
 })
-} catch {
 }
