30e6ae3e3d7ce2c3734a2e1e034b749dceee0f54
Kitten Films <kittenapps@icloud.com>
2025-02-23 13:07:23 +0500

    Update up2.js

--- up2.js
+++ up2.js
@@ -1,2 +1,1 @@
-//import update() from "/up.js"
-var ab = "AB"
+var content = '<html><head></head><body>Hello World 2<script type="module" src="https://kittenapps-films.github.io/up.js"></script></body></html>'
