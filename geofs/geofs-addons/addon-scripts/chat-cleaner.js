573b058406162032788dd455f259c9250463f3b9
Kitten Films <kittenapps@icloud.com>
2025-02-24 11:27:35 +0500

    Update chat-cleaner.js

--- geofs/geofs-addons/addon-scripts/chat-cleaner.js
+++ geofs/geofs-addons/addon-scripts/chat-cleaner.js
@@ -1,5 +1,5 @@
 'use strict'
-alert("There is a newer version of this addon, go to Github to get it");
+alert("There is a newer version of the chat cleaner addon, go to Github to get it");
 setTimeout(function(){
     console.log("Chat running");
     setInterval(clean, 100);
