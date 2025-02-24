b70b843d7ce41282b93a08a92a46f3194d7cb6ce
Kitten Films <kittenapps@icloud.com>
2025-02-24 20:00:03 +0500

    Update saving.js

--- edit/saving.js
+++ edit/saving.js
@@ -7,9 +7,8 @@ setInterval(checkURL('https://kittenapps-films.github.io/GeoFS_Wiki/'+d), 500);
 async function checkURL(url) {
   try {
     const response = await fetch(url);
-    if (!response.ok) return console.log(`URL does not exist: ${url}`);
-    window.location.replace('https://kittenapps-films.github.io/GeoFS_Wiki/'+d);
-  } catch (error) {
     //go again
+  } catch (error) {
+    window.location.replace('https://kittenapps-films.github.io/GeoFS_Wiki/'+d);
   }
 }
