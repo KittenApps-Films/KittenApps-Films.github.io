fffce161d501bea58d6fd63720c388d26240dc35
Kitten Films <kittenapps@icloud.com>
2025-03-02 12:34:01 +0500

    Update code.js

--- edit/code.js
+++ edit/code.js
@@ -1,11 +1,10 @@
 import { one } from '/code/one.js';
 import { two } from '/code/two.js';
 import { three } from '/code/three.js';
-export var full = one+two+three
+var full = one+two+three
 let path = window.location.href.split('?')[0]
-export function fuller() {
-console.log(full, path)
+export function get() {
   if (path == "https://kittenapps-films.github.io/edit/makeWiki.html") {
-    return "full"
+    return full
   }
 }
