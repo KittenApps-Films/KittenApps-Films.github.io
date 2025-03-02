a8c1094bf6d9640d48248c5721f9ff6be6931ff8
Kitten Films <kittenapps@icloud.com>
2025-03-02 12:06:00 +0500

    Update code.js

--- edit/code.js
+++ edit/code.js
@@ -2,3 +2,6 @@ import { one } from '/code/one.js';
 import { two } from '/code/two.js';
 import { three } from '/code/three.js';
 export var full = one+two+three
+export function fuller() {
+console.log(full)
+}
