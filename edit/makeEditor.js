846028cd21c6a0c758c325d94ca689846f3ed549
Kitten Films <kittenapps@icloud.com>
2025-03-02 12:57:59 +0500

    Update makeEditor.js

--- edit/makeEditor.js
+++ edit/makeEditor.js
@@ -1,1 +1,16 @@
+var target = document.querySelector("#content");
+var div = document.getElementById("preview")
 
+var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+      div.innerHTML = target.innerHTML
+      });
+});
+
+var config = {
+  childList: true,
+  subtree: true,
+  characterData: true
+};
+
+observer.observe(target, config);
